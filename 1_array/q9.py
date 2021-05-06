767c5d835bf59a01623929c3e644c3564b1e1ab9
wanli <lingleeabd@126.com>
2021-05-06 08:43:40 +0800

    高度检查器

---
+++ 1_array/q9.py
@@ -0,0 +1,23 @@
+"""
+高度检查器
+学校在拍年度纪念照时，一般要求学生按照 非递减 的高度顺序排列。
+请你返回能让所有学生以 非递减 高度排列的最小必要移动人数。
+注意，当一组学生被选中时，他们之间可以以任何可能的方式重新排序，而未被选中的学生应该保持不动。
+输入：heights = [1,1,4,2,1,3]
+输出：3
+解释：
+当前数组：[1,1,4,2,1,3]
+目标数组：[1,1,1,2,3,4]
+"""
+
+
+def check_high(heights):
+    new_heights = sorted(heights) # 通过该方式复制一个list
+    res = 0
+    for i in range(len(new_heights)):
+        if heights[i] == new_heights[i]:
+            res += 1
+    return res
+
+
+print(check_high([1,1,4,2,1,3]))
